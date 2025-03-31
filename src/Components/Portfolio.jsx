2484eaecfe65121cd5bb477ec5a6ff4e2877db78
Bittu Kumar <bk901277@gmail.com>
2025-03-31 22:28:26 +0530

    Work on Portfolio page

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,247 +1,240 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./NavBar";
+import pojt01 from '../img/project-1.jpg';
+import pojt02 from '../img/project-2.png';
+import pojt03 from '../img/project-3.jpg';
+import pojt04 from '../img/project-4.png';
+import pojt05 from '../img/project-5.png';
+import pojt06 from '../img/project-6.png';
+import pojt07 from '../img/project-7.png';
+import pojt08 from '../img/project-8.jpg';
+import pojt09 from '../img/project-9.png';
 
 function Portfolio() {
+  useEffect(() => {
+    const filterItems = document.querySelectorAll("[data-filter-item]");
+    const filterButtons = document.querySelectorAll("[data-filter-btn]");
+
+    const filterFunc = (selectedValue) => {
+      filterItems.forEach((item) => {
+        if (selectedValue === "all" || selectedValue === item.dataset.category) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    };
+
+    filterButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        // Remove active class from all buttons
+        filterButtons.forEach((btn) => btn.classList.remove("active"));
+        
+        // Add active class to the clicked button
+        this.classList.add("active");
+
+        // Filter projects
+        filterFunc(this.textContent.toLowerCase());
+      });
+    });
+
+    // Cleanup function to remove event listeners
+    return () => {
+      filterButtons.forEach((button) => {
+        button.removeEventListener("click", filterFunc);
+      });
+    };
+  }, []);
+
   return (
     <>
       <div className="main-content">
-      <Navbar/>
+        <Navbar />
         <article className="portfolio active" data-page="portfolio">
           <header>
             <h2 className="h2 article-title">Portfolio</h2>
           </header>
 
           <section className="projects">
+            {/* Filter Buttons */}
             <ul className="filter-list">
               <li className="filter-item">
-                <button className="active" data-filter-btn>
-                  All
-                </button>
+                <button className="active" data-filter-btn>All</button>
               </li>
-
               <li className="filter-item">
                 <button data-filter-btn>Web design</button>
               </li>
-
               <li className="filter-item">
                 <button data-filter-btn>Applications</button>
               </li>
-
               <li className="filter-item">
                 <button data-filter-btn>Web development</button>
               </li>
             </ul>
 
-            <div className="filter-select-box">
-              <button className="filter-select" data-select>
-                <div className="select-value" data-selecct-value>
-                  Select category
-                </div>
-
-                <div className="select-icon">
-                  <ion-icon name="chevron-down"></ion-icon>
-                </div>
-              </button>
-
-              <ul className="select-list">
-                <li className="select-item">
-                  <button data-select-item>All</button>
-                </li>
-
-                <li className="select-item">
-                  <button data-select-item>Web design</button>
-                </li>
-
-                <li className="select-item">
-                  <button data-select-item>Applications</button>
-                </li>
-
-                <li className="select-item">
-                  <button data-select-item>Web development</button>
-                </li>
-              </ul>
-            </div>
-
+            {/* Project List */}
             <ul className="project-list">
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web development"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+            <li class="project-item  active" data-filter-item data-category="web development">
+              <a href="#">
 
-                    {/* <img src="./assets/images/project-1.jpg" alt="finance" loading="lazy"> */}
-                  </figure>
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
 
-                  <h3 className="project-title">Finance</h3>
+                  <img src={pojt01} alt="finance" loading="lazy"/>
+                </figure>
 
-                  <p className="project-category">Web development</p>
-                </a>
-              </li>
+                <h3 class="project-title">Finance</h3>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web development"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+                <p class="project-category">Web development</p>
 
-                    {/* <img src="./assets/images/project-2.png" alt="orizon" loading="lazy"> */}
-                  </figure>
+              </a>
+            </li>
 
-                  <h3 className="project-title">Orizon</h3>
+            <li class="project-item  active" data-filter-item data-category="web development">
+              <a href="#">
 
-                  <p className="project-category">Web development</p>
-                </a>
-              </li>
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web design"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+                  <img src={pojt02} loading="lazy"/>
+                </figure>
 
-                    {/* <img src="./assets/images/project-3.jpg" alt="fundo" loading="lazy"> */}
-                  </figure>
+                <h3 class="project-title">Orizon</h3>
 
-                  <h3 className="project-title">Fundo</h3>
+                <p class="project-category">Web development</p>
 
-                  <p className="project-category">Web design</p>
-                </a>
-              </li>
+              </a>
+            </li>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="applications"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+            <li class="project-item  active" data-filter-item data-category="web design">
+              <a href="#">
 
-                    {/* <img src="./assets/images/project-4.png" alt="brawlhalla" loading="lazy"> */}
-                  </figure>
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
 
-                  <h3 className="project-title">Brawlhalla</h3>
+                  <img src={pojt03} loading="lazy"/>
+                </figure>
 
-                  <p className="project-category">Applications</p>
-                </a>
-              </li>
+                <h3 class="project-title">Fundo</h3>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web design"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+                <p class="project-category">Web design</p>
 
-                    {/* <img src="./assets/images/project-5.png" alt="dsm." loading="lazy"> */}
-                  </figure>
+              </a>
+            </li>
 
-                  <h3 className="project-title">DSM.</h3>
+            <li class="project-item  active" data-filter-item data-category="applications">
+              <a href="#">
 
-                  <p className="project-category">Web design</p>
-                </a>
-              </li>
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web design"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+                  <img src={pojt04} loading="lazy"/>
+                </figure>
 
-                    {/* <img src="./assets/images/project-6.png" alt="metaspark" loading="lazy"> */}
-                  </figure>
+                <h3 class="project-title">Brawlhalla</h3>
 
-                  <h3 className="project-title">MetaSpark</h3>
+                <p class="project-category">Applications</p>
 
-                  <p className="project-category">Web design</p>
-                </a>
-              </li>
+              </a>
+            </li>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web development"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+            <li class="project-item  active" data-filter-item data-category="web design">
+              <a href="#">
 
-                    {/* <img src="./assets/images/project-7.png" alt="summary" loading="lazy"> */}
-                  </figure>
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
 
-                  <h3 className="project-title">Summary</h3>
+                  <img src={pojt05} loading="lazy"/>
+                </figure>
 
-                  <p className="project-category">Web development</p>
-                </a>
-              </li>
+                <h3 class="project-title">DSM.</h3>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="applications"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+                <p class="project-category">Web design</p>
 
-                    {/* <img src="./assets/images/project-8.jpg" alt="task manager" loading="lazy"> */}
-                  </figure>
+              </a>
+            </li>
 
-                  <h3 className="project-title">Task Manager</h3>
+            <li class="project-item  active" data-filter-item data-category="web design">
+              <a href="#">
 
-                  <p className="project-category">Applications</p>
-                </a>
-              </li>
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
 
-              <li
-                className="project-item  active"
-                data-filter-item
-                data-category="web development"
-              >
-                <a href="#">
-                  <figure className="project-img">
-                    <div className="project-item-icon-box">
-                      <ion-icon name="eye-outline"></ion-icon>
-                    </div>
+                  <img src={pojt06} loading="lazy"/>
+                </figure>
 
-                    {/* <img src="./assets/images/project-9.png" alt="arrival" loading="lazy"> */}
-                  </figure>
+                <h3 class="project-title">MetaSpark</h3>
 
-                  <h3 className="project-title">Arrival</h3>
+                <p class="project-category">Web design</p>
 
-                  <p className="project-category">Web development</p>
-                </a>
-              </li>
+              </a>
+            </li>
+
+            <li class="project-item  active" data-filter-item data-category="web development">
+              <a href="#">
+
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
+
+                  <img src={pojt07} loading="lazy"/>
+                </figure>
+
+                <h3 class="project-title">Summary</h3>
+
+                <p class="project-category">Web development</p>
+
+              </a>
+            </li>
+
+            <li class="project-item  active" data-filter-item data-category="applications">
+              <a href="#">
+
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
+
+                  <img src={pojt08} loading="lazy"/>
+                </figure>
+
+                <h3 class="project-title">Task Manager</h3>
+
+                <p class="project-category">Applications</p>
+
+              </a>
+            </li>
+
+            <li class="project-item  active" data-filter-item data-category="web development">
+              <a href="#">
+
+                <figure class="project-img">
+                  <div class="project-item-icon-box">
+                    <ion-icon name="eye-outline"></ion-icon>
+                  </div>
+
+                  <img src={pojt09} loading="lazy"/>
+                </figure>
+
+                <h3 class="project-title">Arrival</h3>
+
+                <p class="project-category">Web development</p>
+
+              </a>
+            </li>
             </ul>
           </section>
         </article>
@@ -249,6 +242,5 @@ function Portfolio() {
     </>
   );
 }
-
 
 export default Portfolio;
